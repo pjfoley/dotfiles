041ba62ba8373aad728eb8f1f3e737c29cedd4bc
Peter Foley <peter@ifoley.id.au>
2013-07-22 23:35:31 +1000

    Clone now created default dotfiles settings

--- bin/clone.sh
+++ bin/clone.sh
@@ -1,15 +1,40 @@
 #!/bin/bash
 
-l_DOTFILES_GIT_REMOTE="git@github.com:pjfoley/dotfiles.git"
-l_DOTFILES_DIRECTORY="${HOME}/.dotfiles"
+[[ ! -d "~/default" ]] && mkdir -p "~/default"
+
+[[ ! -e ~/default/dotfiles ]] && cat << 'EOF' > ~/default/dotfiles
+# Defaults for dotfiles - sourced by various dotfiles scripts
+
+# Location of dotfiles GIT Repo
+DOTFILES_GIT_REPO="${DOTFILES_GIT_REPO:=git@github.com:pjfoley/dotfiles.git}"
+
+# User's Home Location
+USER_HOME="${USER_HOME:=${HOME}}"
+
+# Location to store dotfiles
+DOTFILES_LOC="${DOTFILES_LOC:=${USER_HOME}/.dotfiles}"
+
+# Location of the Home Directory files withing the dotfiles repo
+DOTFILES_SRC="${DOTFILES_SRC:=${DOTFILES_LOC}/home}"
+
+# Backup existing dotfiles if it will be replaced
+DOTFILES_BKUP="${DOTFILES_BKUP:=${USER_HOME}/.dotfiles_org}"
+
+# Do we want to backup existing dotfiles
+BACKUP=true
+
+# Setup Dotfiles location for GIT variables
+DOTFILES_GIT_DIR="${DOTFILES_GIT_DIR:=${DOTFILES_LOC}/.git}"
+DOTFILES_GIT_WORK_TREE="${DOTFILES_GIT_WORK_TREE:=${DOTFILES_LOC}}"
+EOF
 
 l_CURRENT_DIR=${PWD}
 
 # Install the GIT Repo
-if [[ ! -d ${l_DOTFILES_DIRECTORY} ]]; then
-  git clone ${l_DOTFILES_GIT_REMOTE} ${l_DOTFILES_DIRECTORY}
+if [[ ! -d ${DOTFILES_LOC} ]]; then
+  git clone ${DOTFILES_GIT_REPO} ${DOTFILES_LOC}
 
-  cd ${l_DOTFILES_DIRECTORY}
+  cd ${DOTFILES_LOC}
 
   # Update submodules
   git submodule update --recursive --init
